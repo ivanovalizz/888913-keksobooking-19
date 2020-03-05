a8df034458702e28231d22fed9c70dc67e5a4cad
ivanovalizz <elizaveta.sm97@gmail.com>
2020-03-05 12:07:30 +0300

    Add drag-and-frop to the main pin

---
+++ js/mainPin.js
@@ -0,0 +1,53 @@
+'use strict';
+
+(function () {
+  var mapPinMainElement = document.querySelector('.map__pin--main');
+
+  mapPinMainElement.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      dragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      mapPinMainElement.style.top = (mapPinMainElement.offsetTop - shift.y) + 'px';
+      mapPinMainElement.style.left = (mapPinMainElement.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      if (dragged) {
+        var onClickPreventDefault = function (clickEvt) {
+          clickEvt.preventDefault();
+          mapPinMainElement.removeEventListener('click', onClickPreventDefault)
+        };
+        mapPinMainElement.addEventListener('click', onClickPreventDefault);
+      }
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+})();
